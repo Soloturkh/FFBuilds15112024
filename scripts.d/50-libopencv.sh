ba8fbb56c9d38751a5783411fc7cb0249132afcd
Soloturkh <127161170+Soloturkh@users.noreply.github.com>
2024-11-16 23:46:33 +0300

    Update 50-libopencv.sh

--- scripts.d/50-libopencv.sh
+++ scripts.d/50-libopencv.sh
@@ -2,47 +2,90 @@
 
 SCRIPT_REPO="https://github.com/opencv/opencv.git"
 SCRIPT_COMMIT="4.x"
-SCRIPT_REPO_CONTRIB="https://github.com/opencv/opencv_contrib.git"
+SCRIPT_TAGFILTER="4.x"
 
 ffbuild_enabled() {
+    # Burada özel bir kontrol yapılmaz, her hedefe uygun destek sağlanır.
     return 0
 }
 
 ffbuild_dockerdl() {
-    echo "Cloning OpenCV repositories..."
-    git clone --depth=1 -b "$SCRIPT_COMMIT" "$SCRIPT_REPO" opencv
-    git clone --depth=1 -b "$SCRIPT_COMMIT" "$SCRIPT_REPO_CONTRIB" opencv_contrib
+    # Gerekli dosyaların indirilmesi için komut
+    default_dl .
+    echo "git submodule update --init --recursive --depth=1"
 }
 
 ffbuild_dockerbuild() {
-    echo "Building OpenCV..."
-    cd opencv
-    mkdir -p build && cd build
+    # OpenCV için yapılandırma seçeneklerini belirleyelim
+    local myconf=(
+        --disable-cli
+        --enable-static
+        --enable-pic
+        --disable-avs
+        --disable-swscale
+        --disable-lavf
+        --disable-ffms
+        --disable-gpac
+        --disable-lsmash
+        --extra-asflags="-w-macro-params-legacy"
+        --extra-cflags="-Wno-error=incompatible-pointer-types"
+        --prefix="$FFBUILD_PREFIX"
+    )
 
-    cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" \
-          -DCMAKE_BUILD_TYPE=Release \
-          -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
-          -DBUILD_SHARED_LIBS=OFF \
-          -DBUILD_EXAMPLES=OFF \
-          -DBUILD_TESTS=OFF \
-          -DBUILD_PERF_TESTS=OFF \
-          -DWITH_FFMPEG=ON \
-          -DWITH_OPENMP=ON \
-          -DWITH_IPP=OFF \
-          -DWITH_PROTOBUF=OFF \
-          -DOPENCV_EXTRA_MODULES_PATH="../opencv_contrib/modules" \
-          -DENABLE_CXX11=ON \
-          ..
+    # Platform bazlı hedef yapılandırmaları ekleyelim
+    if [[ $TARGET == win64 ]]; then
+        myconf+=(
+            --host="$FFBUILD_TOOLCHAIN"
+            --cross-prefix="$FFBUILD_CROSS_PREFIX"
+            mingw64
+        )
+    elif [[ $TARGET == win32 ]]; then
+        myconf+=(
+            --host="$FFBUILD_TOOLCHAIN"
+            --cross-prefix="$FFBUILD_CROSS_PREFIX"
+            mingw
+        )
+    elif [[ $TARGET == linux64 ]]; then
+        myconf+=(
+            --host="$FFBUILD_TOOLCHAIN"
+            --cross-prefix="$FFBUILD_CROSS_PREFIX"
+            linux-x86_64
+        )
+    elif [[ $TARGET == linuxarm64 ]]; then
+        myconf+=(
+            --host="$FFBUILD_TOOLCHAIN"
+            --cross-prefix="$FFBUILD_CROSS_PREFIX"
+            linux-aarch64
+        )
+    else
+        echo "Unknown target: $TARGET"
+        return -1
+    fi
 
+    # Derleme ayarları
+    export CFLAGS="$CFLAGS -fno-strict-aliasing"
+    export CXXFLAGS="$CXXFLAGS -fno-strict-aliasing"
+
+    # OpenCV derleme için uygun C ve C++ derleyicilerini ayarlayalım
+    export CC="${CC/${FFBUILD_CROSS_PREFIX}/}"
+    export CXX="${CXX/${FFBUILD_CROSS_PREFIX}/}"
+    export AR="${AR/${FFBUILD_CROSS_PREFIX}/}"
+    export RANLIB="${RANLIB/${FFBUILD_CROSS_PREFIX}/}"
+
+    # Yapılandırma komutu
+    ./configure "${myconf[@]}"
+
+    # Makefile'de CFLAGS ve LDFLAGS ayarlamalarını yapalım
+    sed -i -e "/^CFLAGS=/s|=.*|=${CFLAGS}|" -e "/^LDFLAGS=/s|=[[:space:]]*$|=${LDFLAGS}|" Makefile
+
+    # Derleme ve yükleme işlemleri
     make -j$(nproc)
     make install
-
-    # pkg-config dosyasını güncelle
-    echo "Libs.private: -lm -lpthread -ldl -lz" >> "$FFBUILD_PREFIX"/lib/pkgconfig/opencv4.pc
-    echo "Cflags.private: -DOPENCV_STATIC" >> "$FFBUILD_PREFIX"/lib/pkgconfig/opencv4.pc
 }
 
 ffbuild_configure() {
+    # win* hedefleri için libopencv'yi etkinleştir
+    [[ $TARGET == win* ]] && return 0
     echo --enable-libopencv
 }
 
