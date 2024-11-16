1f54ac197d4614dd2ed598ae5854eee1644e6fb6
Soloturkh <127161170+Soloturkh@users.noreply.github.com>
2024-11-16 21:59:44 +0300

    Create 50-libopencv.sh

---
+++ scripts.d/50-libopencv.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+SCRIPT_REPO="https://github.com/opencv/opencv.git"
+SCRIPT_COMMIT="4.x"
+
+SCRIPT_REPO_CONTRIB="https://github.com/opencv/opencv_contrib.git"
+
+ffbuild_enabled() {
+    return 0
+}
+
+ffbuild_dockerdl() {
+    echo "Cloning OpenCV repositories..."
+    git-mini-clone "$SCRIPT_REPO" "$SCRIPT_COMMIT" opencv
+    git-mini-clone "$SCRIPT_REPO_CONTRIB" "$SCRIPT_COMMIT" opencv_contrib
+}
+
+ffbuild_dockerbuild() {
+    echo "Building OpenCV..."
+    cd opencv
+    mkdir build && cd build
+
+    cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" \
+          -DCMAKE_BUILD_TYPE=Release \
+          -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
+          -DBUILD_SHARED_LIBS=OFF \
+          -DBUILD_EXAMPLES=OFF \
+          -DBUILD_TESTS=OFF \
+          -DBUILD_PERF_TESTS=OFF \
+          -DWITH_FFMPEG=ON \
+          -DWITH_OPENMP=ON \
+          -DWITH_IPP=OFF \
+          -DWITH_1394=OFF \
+          -DWITH_PROTOBUF=ON \  # Protobuf devre dışı bırakılmadı
+          -DOPENCV_EXTRA_MODULES_PATH="../opencv_contrib/modules" \
+          -DENABLE_CXX11=ON \
+          ..
+
+    make -j$(nproc)
+    make install
+
+    # pkg-config dosyasını güncelle
+    echo "Libs.private: -lm -lpthread -ldl -lz" >> "$FFBUILD_PREFIX"/lib/pkgconfig/opencv4.pc
+    echo "Cflags.private: -DOPENCV_STATIC" >> "$FFBUILD_PREFIX"/lib/pkgconfig/opencv4.pc
+}
+
+ffbuild_configure() {
+    echo --enable-libopencv
+}
+
+ffbuild_unconfigure() {
+    echo --disable-libopencv
+}
