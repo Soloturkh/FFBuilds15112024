e00df60ed0e91cb27f779d6a43453937709b94c4
Soloturkh <127161170+Soloturkh@users.noreply.github.com>
2024-11-17 03:02:16 +0300

    Update 50-libopencv.sh

--- scripts.d/50-libopencv.sh
+++ scripts.d/50-libopencv.sh
@@ -2,8 +2,8 @@
 
 # OpenCV deposu ve versiyon bilgileri
 SCRIPT_REPO="https://github.com/opencv/opencv.git"
+SCRIPT_CONTRIB_REPO="https://github.com/opencv/opencv_contrib.git"
 SCRIPT_COMMIT="5.x"  # OpenCV'nin 5.x sürümünü kullanıyoruz
-SCRIPT_TAGFILTER="5.x"
 
 # Bu fonksiyon tüm hedefler için etkin
 ffbuild_enabled() {
@@ -12,26 +12,28 @@ ffbuild_enabled() {
 
 # Gerekli dosyaları indirir
 ffbuild_dockerdl() {
+    echo "OpenCV kaynak kodu indiriliyor..."
     git clone --depth 1 --branch "$SCRIPT_COMMIT" "$SCRIPT_REPO" opencv
-    git -C opencv submodule update --init --recursive --depth=1
+    git clone --depth 1 --branch "$SCRIPT_COMMIT" "$SCRIPT_CONTRIB_REPO" opencv_contrib
+    echo "OpenCV indirildi."
 }
 
 # OpenCV'yi yapılandırır ve derler
 ffbuild_dockerbuild() {
-    # OpenCV için gerekli bağımlılıkları kur
+    echo "Bağımlılıklar kuruluyor..."
     apt-get update && apt-get install -y --no-install-recommends \
         build-essential cmake pkg-config wget unzip \
         libjpeg-turbo8-dev libpng-dev libtiff-dev \
         libavcodec-dev libavformat-dev libswscale-dev \
-        libxvidcore-dev libx264-dev libgtk2.0-dev libatlas-base-dev \
+        libxvidcore-dev libx264-dev libgtk-3-dev libatlas-base-dev \
         gfortran python3-dev python3-numpy libfreetype6-dev \
         libharfbuzz-dev libcurl4-openssl-dev libssl-dev && \
         rm -rf /var/lib/apt/lists/*
 
-    # OpenCV'nin yapılandırması için ayarları tanımla
+    echo "OpenCV yapılandırılıyor..."
     local myconf=(
         -D CMAKE_BUILD_TYPE=Release
-        -D CMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX"
+        -D CMAKE_INSTALL_PREFIX=/usr/local
         -D BUILD_SHARED_LIBS=OFF
         -D BUILD_EXAMPLES=OFF
         -D BUILD_TESTS=OFF
@@ -41,7 +43,7 @@ ffbuild_dockerbuild() {
         -D WITH_IPP=OFF
         -D WITH_PROTOBUF=OFF
         -D ENABLE_CXX11=ON
-        -D BUILD_PKG_CONFIG=ON
+        -D OPENCV_EXTRA_MODULES_PATH="../opencv_contrib/modules"
         -D OPENCV_ENABLE_PKG_CONFIG=ON
         -D OPENCV_GENERATE_PKGCONFIG=ON
     )
@@ -58,12 +60,12 @@ ffbuild_dockerbuild() {
         linux64|linuxarm64)
             myconf+=(
                 -D CMAKE_SYSTEM_NAME=Linux
-                -D CMAKE_C_COMPILER="$FFBUILD_TOOLCHAIN/gcc"
-                -D CMAKE_CXX_COMPILER="$FFBUILD_TOOLCHAIN/g++"
+                -D CMAKE_C_COMPILER=gcc
+                -D CMAKE_CXX_COMPILER=g++
             )
             ;;
         *)
-            echo "Unknown target: $TARGET"
+            echo "Bilinmeyen hedef: $TARGET"
             return 1
             ;;
     esac
@@ -72,30 +74,15 @@ ffbuild_dockerbuild() {
     export CFLAGS="$CFLAGS -fno-strict-aliasing"
     export CXXFLAGS="$CXXFLAGS -fno-strict-aliasing"
 
-    # pkg-config yapılandırması
-    PKG_CONFIG_PATH="$FFBUILD_PREFIX/lib/pkgconfig"
-    mkdir -p "$PKG_CONFIG_PATH"
-    cat > "$PKG_CONFIG_PATH/opencv.pc" <<EOF
-prefix=$FFBUILD_PREFIX
-exec_prefix=\${prefix}
-libdir=\${exec_prefix}/lib
-includedir=\${prefix}/include
-
-Name: OpenCV
-Description: OpenCV - Open Source Computer Vision Library
-Version: 5.x
-Cflags: -I\${includedir}
-Libs: -L\${libdir} -lopencv_core -lopencv_imgproc -lopencv_highgui
-EOF
-
-    # Build dizinini oluştur ve yapılandır
+    # Yapılandırma ve derleme
     mkdir -p opencv/build
     cd opencv/build
     cmake .. "${myconf[@]}"
 
-    # Derleme ve yükleme işlemleri
+    echo "OpenCV derleniyor..."
     make -j$(nproc)
     make install
+    echo "OpenCV kurulumu tamamlandı."
 }
 
 # Yapılandırma ve devre dışı bırakma işlemleri
