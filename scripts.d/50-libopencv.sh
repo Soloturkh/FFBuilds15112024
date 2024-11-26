f89fcbc9cf6178cafaee30a27044fd364a153c34
Soloturkh <127161170+Soloturkh@users.noreply.github.com>
2024-11-26 04:31:07 +0300

    Update 50-libopencv.sh

--- scripts.d/50-libopencv.sh
+++ scripts.d/50-libopencv.sh
@@ -17,58 +17,143 @@ ffbuild_dockerdl() {
 }
 
 ffbuild_dockerbuild() {
-    apt update && apt install -y --no-install-recommends \
-    build-essential cmake git unzip pkg-config \
-    libjpeg-dev libpng-dev libtiff-dev \
-    libavcodec-dev libavformat-dev libswscale-dev \
-    libv4l-dev libxvidcore-dev libx264-dev \
-    libgtk-3-dev libcanberra-gtk* libatlas-base-dev gfortran \
-    python3-dev python3-numpy
+
+	apt update
+	apt upgrade
 	
+	#Generic tools
+	apt install -y build-essential cmake pkg-config unzip yasm git checkinstall
+	#image i/o Libs
+	apt install -y libjpeg-dev libpng-dev libtiff-dev
+	#Video/Audio Libs
+	# Install basic codec libraries
+	apt install -y libavcodec-dev libavformat-dev libswscale-dev
+
+	# Install GStreamer development libraries
+	apt install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
+
+	# Install additional codec and format libraries
+	apt install -y libxvidcore-dev libx264-dev libopus-dev libv4l-dev
+
+	# Install additional audio codec libraries
+	apt install -y libmp3lame-dev libvorbis-dev
+
+	# Install FFmpeg (which includes libavresample functionality)
+	apt install -y ffmpeg
+
+	# Optional: Install VA-API for hardware acceleration
+	apt install -y libva-dev
+	
+	# Install video capture libraries and utilities
+    apt install -y libdc1394-25 libdc1394-dev libxine2-dev libv4l-dev v4l-utils
+	
+	#Create a symbolic link for video device header
+	ln -s /usr/include/libv4l1-videodev.h /usr/include/linux/videodev.h
+	
+	#GTK lib for the graphical user functionalites coming from OpenCV highghui module
+	apt install -y libgtk-3-dev
+	#Parallelism library C++ for CPU
+	apt install -y libtbb-dev
+	#Optimization libraries for OpenCV
+	apt install -y libatlas-base-dev gfortran
+	#Optional libraries:
+	apt install -y libprotobuf-dev protobuf-compiler
+	apt install -y libgoogle-glog-dev libgflags-dev
+	apt install -y libgphoto2-dev libeigen3-dev libhdf5-dev doxygen
+	apt install -y libgtk-3-dev libcanberra-gtk* libatlas-base-dev python3-dev python3-numpy
+
     mkdir build && cd build
+	
+	# NVIDIA GPU'nun mevcut olup olmadığını kontrol edin
+	# NVIDIA GPU'nun mevcut olup olmadığını kontrol edin
+	if command -v nvidia-smi &> /dev/null; then
+		echo "NVIDIA GPU algılandı, CUDA desteği ve TBB devre dışı bırakılıyor..."
+		GPU_OPTIONS="-DWITH_CUDA=ON \
+					  -DWITH_CUDNN=ON \
+					  -DOPENCV_DNN_CUDA=ON \
+					  -DCUDA_ARCH_BIN=ALL \
+					  -DENABLE_FAST_MATH=ON \
+					  -DCUDA_FAST_MATH=ON \
+					  -DWITH_CUBLAS=ON \
+					  -DBUILD_opencv_cudacodec=ON"
+		TBB_OPTION="-DWITH_TBB=OFF"  # GPU mevcutsa TBB devre dışı
+	else
+		echo "NVIDIA GPU algılanmadı, sadece CPU desteği etkinleştiriliyor..."
+		GPU_OPTIONS="-DWITH_CUDA=OFF \
+					  -DWITH_CUDNN=OFF \
+					  -DOPENCV_DNN_CUDA=OFF \
+					  -DWITH_CUBLAS=OFF \
+					  -DBUILD_opencv_cudacodec=OFF"
+		TBB_OPTION="-DWITH_TBB=ON"  # GPU yoksa TBB etkin
+	fi
 
     cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" \
           -DCMAKE_BUILD_TYPE=Release \
           -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
           -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules \
           -DENABLE_PRECOMPILED_HEADERS=OFF \
-          -DBUILD_SHARED_LIBS=OFF \
+          -DBUILD_EXAMPLES=OFF \
           -DBUILD_TESTS=OFF \
           -DBUILD_PERF_TESTS=OFF \
-          -DWITH_FFMPEG=ON \
-          -DWITH_OPENMP=ON \
+		   $GPU_OPTIONS \
+		   $TBB_OPTION \
+          -DWITH_OPENCL=OFF \
+          -DWITH_V4L=OFF \
+		  -DWITH_FFMPEG=OFF \
+		  -DWITH_GSTREAMER=OFF \
+          -DWITH_MSMF=OFF \
+		  -DWITH_DSHOW=OFF \
+		  -DWITH_AVFOUNDATION=OFF \
+		  -DWITH_1394=OFF \
           -DWITH_IPP=OFF \
           -DWITH_PROTOBUF=OFF \
           -DENABLE_CXX11=ON \
           -DBUILD_PKG_CONFIG=ON \
           -DOPENCV_ENABLE_PKG_CONFIG=ON \
           -DOPENCV_GENERATE_PKGCONFIG=ON \
-          -DBUILD_EXAMPLES=OFF ..
+          -DOPENCV_PC_FILE_NAME=opencv.pc \
+          -DOPENCV_ENABLE_NONFREE=ON \
+          -DBUILD_EXAMPLES=OFF \
+		  -DINSTALL_PYTHON_EXAMPLES=OFF \
+		  -DINSTALL_C_EXAMPLES=OFF \
+		  -DBUILD_ZLIB=ON \
+		  -DBUILD_SHARED_LIBS=OFF ..
+   
     make -j$(nproc)
-    make install
+	make install
     ldconfig
+	
+	#Include the libs in your environment
+	echo "/usr/local/lib" >> /etc/ld.so.conf.d/opencv.conf
+	ldconfig
+	
+	
+	# OpenCV'nin başarıyla kurulduğunu kontrol et
+	echo "OpenCV versiyonu kontrol ediliyor..."
+	PKG_CONFIG_PATH="$FFBUILD_PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH"
+	export PKG_CONFIG_PATH
 
-	#cat <<EOF >opencv4.pc
-	#prefix=$FFBUILD_PREFIX
-	#exec_prefix=\${prefix}
-	#libdir=\${exec_prefix}/lib
-	#includedir=\${prefix}/include/opencv4
-	#
-	#Name: OpenCV
-	#Description: Open Source Computer Vision Library
-	#Version: $OPENCV_VERSION
-	#Libs: -L\${exec_prefix}/lib -lopencv_gapi -lopencv_stitching -lopencv_aruco -lopencv_bgsegm -lopencv_bioinspired -lopencv_ccalib -lopencv_dnn_objdetect -lopencv_dnn_superres -lopencv_dpm -lopencv_face -lopencv_freetype -lopencv_fuzzy -lopencv_hfs -lopencv_img_hash -lopencv_intensity_transform -lopencv_line_descriptor -lopencv_mcc -lopencv_quality -lopencv_rapid -lopencv_reg -lopencv_rgbd -lopencv_saliency -lopencv_stereo -lopencv_structured_light -lopencv_phase_unwrapping -lopencv_superres -lopencv_optflow -lopencv_surface_matching -lopencv_tracking -lopencv_highgui -lopencv_datasets -lopencv_text -lopencv_plot -lopencv_videostab -lopencv_videoio -lopencv_wechat_qrcode -lopencv_xfeatures2d -lopencv_shape -lopencv_ml -lopencv_ximgproc -lopencv_video -lopencv_xobjdetect -lopencv_objdetect -lopencv_calib3d -lopencv_imgcodecs -lopencv_features2d -lopencv_dnn -lopencv_flann -lopencv_xphoto -lopencv_photo -lopencv_imgproc -lopencv_core
-	#Libs.private: -L\${exec_prefix}/lib/opencv4/3rdparty -lade -littnotify -llibwebp -llibopenjp2 -lIlmImf -lquirc -L/usr/lib/x86_64-linux-gnu -ljpeg -lpng -ltiff -lz -L/usr/lib/gcc/x86_64-linux-gnu/11 -lgomp -lpthread -lfreetype -lharfbuzz -lIconv::Iconv -ldl -lm -lrt
-	#Cflags: -I\${includedir}
-	#EOF
-    
-    echo "OpenCV versiyonu kontrol ediliyor..."
-    pkg-config --modversion opencv4 || echo "PKG_CONFIG_PATH ayarlarını kontrol edin."
-    echo "OpenCV ${OPENCV_VERSION} başarıyla kuruldu!"
+	if pkg-config --modversion opencv; then
+		echo "OpenCV ${OPENCV_VERSION} başarıyla kuruldu!"
+	else
+		echo "OpenCV kurulumunda sorun var. PKG_CONFIG_PATH ayarlarını kontrol edin."
+		echo "PKG_CONFIG_PATH: $PKG_CONFIG_PATH"
+		exit 1
+	fi
 
-    mkdir -p "$FFBUILD_PREFIX"/lib/pkgconfig
-    cp ./unix-install/opencv4.pc "$FFBUILD_PREFIX"/lib/pkgconfig/
-    cp ./unix-install/opencv4.pc "$FFBUILD_PREFIX"/lib/pkgconfig/libopencv.pc
+	# Pkg-config dosyasını doğru yere kopyalama
+	mkdir -p "$FFBUILD_PREFIX/lib/pkgconfig"
+	# Eğer hedef dizinde dosya zaten varsa, kopyalama işlemi yapmayalım
+	if [ ! -f "$FFBUILD_PREFIX/lib/pkgconfig/opencv.pc" ]; then
+		cp "$FFBUILD_PREFIX/lib/pkgconfig/opencv.pc" "$FFBUILD_PREFIX/lib/pkgconfig/"
+		echo "opencv.pc başarıyla kopyalandı."
+	else
+		echo "opencv.pc zaten mevcut, kopyalanmadı."
+	fi
+	
+	cat "$FFBUILD_PREFIX/lib/pkgconfig/opencv.pc"
+	
+	
 
 }
 
